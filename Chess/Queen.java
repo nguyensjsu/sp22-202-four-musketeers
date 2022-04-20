01744851fed87aaaa9a2e3723d1d058d258218e0
jfantab <john.lu@sjsu.edu>
2022-04-19 18:46:56 -0700

    Clean up code for Queen

--- Chess/Queen.java
+++ Chess/Queen.java
@@ -30,6 +30,9 @@ public class Queen extends ChessPiece {
                 int x = Greenfoot.getMouseInfo().getX();
                 int y = Greenfoot.getMouseInfo().getY();
                 
+                int diffX = x - this.getX();
+                int diffY = y - this.getY();
+                
                 int absDiffX = Math.abs(x - this.getX());
                 int absDiffY = Math.abs(y - this.getY());
                 
@@ -40,124 +43,51 @@ public class Queen extends ChessPiece {
                 {
                     if (x == this.getX())
                     {
-                        int difference = y - this.getY();
+                        int mult = (diffY < 0) ? -1 : 1;
                         
-                        if (difference < 0)
+                        for (int i = 1;i < absDiffY;i++)
                         {
-                            difference = Math.abs(difference);
-                            for (int i = 1;i < difference;i++)
+                            if (!getWorld().getObjectsAt(x,this.getY() + (mult * i),ChessPiece.class).isEmpty())
                             {
-                                if (!getWorld().getObjectsAt(x,this.getY() - i,ChessPiece.class).isEmpty())
-                                {
-                                    occupied = true;
-                                    break;
-                                }
+                                occupied = true;
+                                break;
                             }
                         }
-                        else
-                        {
-                            difference = Math.abs(difference);
-                            for (int i = 1;i < difference;i++)
-                            {
-                                if (!getWorld().getObjectsAt(x,this.getY() + i,ChessPiece.class).isEmpty())
-                                {
-                                    occupied = true;
-                                    break;
-                                }
-                            }
-                        }
-                        if (!occupied)
-                        {
-                            move(x,y);
-                        }
+                        if (!occupied) move(x,y);
+                        
                     }
                     else if (y == this.getY())
                     {
-                        int difference = x - this.getX();
+                        int mult = (diffX < 0) ? -1 : 1;
                         
-                        if (difference < 0)
+                        for (int i = 1; i < absDiffX;i++)
                         {
-                            difference = Math.abs(difference);
-                            for (int i = 1;i < difference;i++)
+                            if (!getWorld().getObjectsAt(this.getX() + (mult * i),y,ChessPiece.class).isEmpty())
                             {
-                                if (!getWorld().getObjectsAt(this.getX() - i,y,ChessPiece.class).isEmpty())
-                                {
-                                    occupied = true;
-                                    break;
-                                }
+                                occupied = true;
+                                break;
                             }
                         }
-                        else
-                        {
-                            difference = Math.abs(difference);
-                            for (int i = 1;i < difference;i++)
-                            {
-                                if (!getWorld().getObjectsAt(this.getX() + i,y,ChessPiece.class).isEmpty())
-                                {
-                                    occupied = true;
-                                    break;
-                                }
-                            }
-                        }
-                        if (!occupied)
-                        {
-                            move(x,y);
-                        }
+                        if (!occupied) move(x,y);
                     }
                     else if (absDiffX == absDiffY)
                     {
-                        int differenceX = x - this.getX();
-                        int differenceY = y - this.getY();
-                        int difference = Math.abs(differenceX);
+                        int multX = 1;
+                        int multY = 1;
                         
-                        if (differenceX < 0 && differenceY < 0)
+                        if (diffX < 0) multX = -1;
+                        if (diffY < 0) multY = -1;
+                            
+                        for (int i = 1; i < absDiffX;i++)
                         {
-                            for (int i = 1;i < difference;i++)
+                            if (!getWorld().getObjectsAt(this.getX() + (multX * i),this.getY() + (multY * i),ChessPiece.class).isEmpty())
                             {
-                                if (!getWorld().getObjectsAt(this.getX() - i,this.getY() - i,ChessPiece.class).isEmpty())
-                                {
-                                    occupied = true;
-                                    break;
-                                }
+                                occupied = true;
+                                break;
                             }
                         }
-                        else if (differenceX < 0 && differenceY > 0)
-                        {
-                            for (int i = 1;i < difference;i++)
-                            {
-                                if (!getWorld().getObjectsAt(this.getX() - i,this.getY() + i,ChessPiece.class).isEmpty())
-                                {
-                                    occupied = true;
-                                    break;
-                                }
-                            }
-                        }
-                        else if (differenceX > 0 && differenceY < 0)
-                        {
-                            for (int i = 1;i < difference;i++)
-                            {
-                                if (!getWorld().getObjectsAt(this.getX() + i,this.getY() - i,ChessPiece.class).isEmpty())
-                                {
-                                    occupied = true;
-                                    break;
-                                }
-                            }
-                        }
-                        else if (differenceX > 0 && differenceY > 0)
-                        {
-                            for (int i = 1;i < difference;i++)
-                            {
-                                if (!getWorld().getObjectsAt(this.getX() + i,this.getY() + i,ChessPiece.class).isEmpty())
-                                {
-                                    occupied = true;
-                                    break;
-                                }
-                            }
-                        }
-                        if (!occupied)
-                        {
-                            move(x,y);
-                        }
+                        
+                        if (!occupied) move(x,y);
                     }
                 }
                 
