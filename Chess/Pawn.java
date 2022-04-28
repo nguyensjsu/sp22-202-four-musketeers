6057ca53ff2795db2d53f96bec2e8c7159860652
Richard Ngo <richardngo04@gmail.com>
2022-04-27 20:24:15 -0700

    Clean up pawn class and fix en passant jump over.

--- Chess/Pawn.java
+++ Chess/Pawn.java
@@ -1,4 +1,6 @@
 import greenfoot.*;
+import java.util.*;
+
 
 public class Pawn extends ChessPiece {
     int stepTwo;
@@ -23,37 +25,113 @@ public class Pawn extends ChessPiece {
         
     }
 
+    private boolean isSpaceEmpty(int x, int y)
+    {
+        return getWorld().getObjectsAt(x,y,ChessPiece.class).isEmpty();
+    }
+
     private void move()
     {
+
+        //check if it's the pawn's turn
         if (ready)
         {
+            //when a mouse is clicked anywhere
             if (Greenfoot.mouseClicked(null))
             {
                 Chessboard chessboard = (Chessboard)getWorld();
                 
+                //get the location of the click
                 int x = Greenfoot.getMouseInfo().getX();
                 int y = Greenfoot.getMouseInfo().getY();
 
-                boolean empty = getWorld().getObjectsAt(x,y,ChessPiece.class).isEmpty();
+                int delta_x = x - this.getX();
+                int delta_y = y - this.getY();
                 
-                if (empty) //if tile is empty or other color
+                System.err.println("Current X:" + this.getX() + " Current Y:" + this.getY());
+                System.err.println("X :" + x + " Y:" + y);            
+
+                //check if the click location contain any chess pieces
+                boolean empty = getWorld().getObjectsAt(x,y,ChessPiece.class).isEmpty();
+
+                //only advancing
+                if(x == this.getX())
                 {
+                    //if first move allow 2 spaces. (need to check 2 spaces ahead to allow move)
+                    if(this.getY() == chessboard.DIM_Y - 2 && delta_y == -2)
+                    {
+                        //check if the 2 spaces in front of it are blank
+                        boolean space1 = this.isSpaceEmpty(x, y);
+                        boolean space2 = this.isSpaceEmpty(x, y+1);
+                        if( space1 && space2 )
+                        {
+                            stepTwo = chessboard.move;
+                            move(x,y);
+                        }
+                    }
+                    //else allow for 1 move forward
+                    else if(delta_y == -1 && this.isSpaceEmpty(x, y))
+                    {
+                        move(x,y);
+                    }
+                }
+                //capturing 
+                else if(Math.abs(delta_x) == 1 && delta_y == -1 )
+                {
+
+
+                    //case 1 capturing a standard peice
+                    //check if the space is not empty and if the piece there is not the same color
+                    if(!isSpaceEmpty(x, y) && getWorld().getObjectsAt(x,y,ChessPiece.class).get(0).color != this.color)
+                    {
+                        move(x,y);
+                    }
+                    //case 2 en passant
+                    else if(chessboard.getObjectsAt(x,y + 1,Pawn.class).size() != 0)
+                    {
+                        Pawn oppPawn = chessboard.getObjectsAt(x,y + 1,Pawn.class).get(0);
+                        if(this.color != oppPawn.color){
+                            if (oppPawn.stepTwo == chessboard.move - 1)
+                            {
+                                move(x,y);
+                                chessboard.removeObject(oppPawn);
+                            }
+                        }
+                    }
+                }
+                    
+                //only capturing
+                    //check if 
+
+
+//removing old pawn code                 
+/*
+                //if tile is empty
+                if (empty) 
+                {
+                    //if first row
                     if (this.getY() == chessboard.DIM_Y - 2)
                     {
+                        //single square moves check
                         if (x == this.getX() && y == this.getY() - 1)
                         {
                             move(x,y);
                         }
+
+                        //allows the pawn to move 2 places on it's first try
                         else if (x == this.getX() && y == this.getY() - 2)
                         {
                             stepTwo = chessboard.move;
                             move(x,y);
                         }
                     }
+                    //standard forward move
                     else if (x == this.getX() && y == this.getY() - 1)
                     {
                         move(x,y);
                     }
+                    
+                    //en passant check x check for attacking y check for advancing.
                     else if ((x == this.getX() - 1 || x == this.getX() + 1) && y == this.getY() - 1)
                     {
                         if (chessboard.getObjectsAt(x,y + 1,Pawn.class).size() != 0)
@@ -69,6 +147,7 @@ public class Pawn extends ChessPiece {
                         }
                     }
                 }
+                //capture move
                 else if (getWorld().getObjectsAt(x,y,ChessPiece.class).get(0).color != this.color) //diff color at pos
                 {
                     if ((x == this.getX() - 1 || x == this.getX() + 1) && y == this.getY() - 1)
@@ -76,7 +155,7 @@ public class Pawn extends ChessPiece {
                         move(x,y);
                     }
                 }
-
+*/
                 ready = false;
             }
         }
